1e474765ce2f6ff7ac027aea71175f4bbf3d8aac
Juan P. Martos <kertacho@gmail.com>
2020-02-15 14:45:06 +0100

    Update conn.php

--- 6. SQL/conn.php
+++ 6. SQL/conn.php
@@ -2,9 +2,9 @@
 function OpenCon()
 {
     $dbhost = "localhost";
-    $dbuser = "daw1920a9";
-    $dbpass = "azGQf@4xqDLG3";
-    $db = "daw1920a9";
+    $dbuser = "";
+    $dbpass = "";
+    $db = "";
     $conn = new PDO("mysql:host=$dbhost;dbname=$db", $dbuser, $dbpass);
 
     return $conn;
