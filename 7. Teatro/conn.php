d4d07426343737d4fd74f2c0c9f8805c1e56fd56
Juan P. Martos <kertacho@gmail.com>
2020-02-15 14:45:22 +0100

    Update conn.php

--- 7. Teatro/conn.php
+++ 7. Teatro/conn.php
@@ -5,9 +5,9 @@ error_reporting(E_ALL);
 function OpenCon()
 {
     $dbhost = "localhost";
-    $dbuser = "daw1920a9";
-    $dbpass = "azGQf@4xqDLG3";
-    $db = "daw1920a9";
+    $dbuser = "";
+    $dbpass = "";
+    $db = "";
     $conn = new PDO("mysql:host=$dbhost;dbname=$db", $dbuser, $dbpass);
 
     return $conn;
